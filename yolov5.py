ed414f3b2a7cc78554823c4e1c9c7b82e300fecf
itachi176 <lhoang17062000@gmail.com>
2021-07-27 20:02:16 +0700

    new obj

--- yolov5.py
+++ yolov5.py
@@ -1,9 +1,12 @@
+from test import CONFIDENCE_THRESHOLD
 import torch 
 import  cv2
 import torchvision
 import os 
 def yolo(image):
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt') 
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='test_best1.pt')
+    model.conf = 0.7
+    model.iou = 0.7
     img1 = image[..., ::-1]
     results = model(img1)
     results.print()  
@@ -12,7 +15,7 @@ def yolo(image):
     results.xywh[0]
     a = results.pandas().xywh[0]
     # print(a)
-    # results.show()
+    results.show()
     # print(a['confidence'][0])
     boxes = []
     return a 
@@ -33,32 +36,32 @@ def yolo(image):
 # cv2.imshow('aaa', img)
 # cv2.waitKey()
 # images = []
-# file = open('test_corr.txt', 'w')
-# for file_name in os.listdir('./new_data'):
-#     # split1 = file_name.split('(')
-#     # name = split1[0]
-#     # split2 = split1[1].split(')')
-#     # split3 = split2[0].split(',')
-#     # x = split3[0]
-#     # y = split3[1]
+file = open('test_corr.txt', 'w')
+for file_name in os.listdir('./new_data'):
+    # split1 = file_name.split('(')
+    # name = split1[0]
+    # split2 = split1[1].split(')')
+    # split3 = split2[0].split(',')
+    # x = split3[0]
+    # y = split3[1]
     
-#     img = cv2.imread(os.path.join("./new_data", file_name))
-#     corr_data = yolo(img)
-#     red_corr = corr_data[corr_data['name'] == 'red']
-#     xcenter_pixel = red_corr['xcenter'].values[0]-607
-#     ycenter_pixel = red_corr['ycenter'].values[0]-25
-#     #67pixel = 3cm 
-#     xcenter_mm = xcenter_pixel/62*30
-#     ycenter_mm = ycenter_pixel/62*30
+    img = cv2.imread(os.path.join("./new_data", file_name))
+    corr_data = yolo(img)
+    red_corr = corr_data[corr_data['name'] == 'red']
+    xcenter_pixel = red_corr['xcenter'].values[0]-607
+    ycenter_pixel = red_corr['ycenter'].values[0]-25
+    #67pixel = 3cm 
+    xcenter_mm = xcenter_pixel/62*30
+    ycenter_mm = ycenter_pixel/62*30
 
-#     # err_x = (int(x)-xcenter_mm)*(int(x)-xcenter_mm)
-#     # err_y = (int(y)-ycenter_mm) * (int(y)-ycenter_mm)
-#     # import math
-#     # err = math.sqrt(err_x+err_y)
-#     w = red_corr['width'].values[0]/62*30
-#     h = red_corr['height'].values[0]/62*30
+    # err_x = (int(x)-xcenter_mm)*(int(x)-xcenter_mm)
+    # err_y = (int(y)-ycenter_mm) * (int(y)-ycenter_mm)
+    # import math
+    # err = math.sqrt(err_x+err_y)
+    w = red_corr['width'].values[0]/62*30
+    h = red_corr['height'].values[0]/62*30
    
-#     # file.writelines([name," ", str(x), " ", str(y), " ", str(xcenter_mm)," ", str(ycenter_mm)," ", str(err_x), " ", str(err_y)," ", str(err)," ", str(w), " ", str(h), "\n"])
-#     # print(file_name.split('(')[0])
-#     file.writelines([file_name, "       ",str(xcenter_mm), "        ", str(ycenter_mm),"\n"])
+    # file.writelines([name," ", str(x), " ", str(y), " ", str(xcenter_mm)," ", str(ycenter_mm)," ", str(err_x), " ", str(err_y)," ", str(err)," ", str(w), " ", str(h), "\n"])
+    # print(file_name.split('(')[0])
+    file.writelines([file_name, "       ",str(xcenter_mm), "        ", str(ycenter_mm),"\n"])
     
