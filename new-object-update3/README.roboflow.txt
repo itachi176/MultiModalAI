7b90b3ffeaf3a058206cd1d5331b1f112158a8af
itachi176 <lhoang17062000@gmail.com>
2021-07-28 14:46:04 +0700

    update

---
+++ new-object-update3/README.roboflow.txt
@@ -0,0 +1,16 @@
+
+data - v3 20210728 242pm
+==============================
+
+This dataset was exported via roboflow.ai on July 28, 2021 at 7:43 AM GMT
+
+It includes 30 images.
+Cells are annotated in YOLO v5 PyTorch format.
+
+The following pre-processing was applied to each image:
+* Auto-orientation of pixel data (with EXIF-orientation stripping)
+* Resize to 1280x720 (Stretch)
+
+No image augmentation techniques were applied.
+
+
--- yolov5.py
+++ yolov5.py
@@ -4,7 +4,7 @@ import  cv2
 import torchvision
 import os 
 def yolo(image):
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='test_best2.pt')
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='new-new-best.pt')
     model.conf = 0.6
     model.iou = 0.6
     img1 = image[..., ::-1]
@@ -15,14 +15,14 @@ def yolo(image):
     results.xywh[0]
     a = results.pandas().xywh[0]
     # print(a)
-    # results.show()
+    results.show()
     # print(a['confidence'][0])
     boxes = []
     return a 
 # x = yolo()
 # y = x[x['name']=='red']
 # print(y['xcenter'].values[0])
-# img = cv2.imread('./new_data/a.jpg')
+# img = cv2.imread('./new-new-data/a.jpg')
 # corr_data = yolo(img)
 # red_corr = corr_data[corr_data['name'] == 'red']
 # xcenter_pixel = red_corr['xcenter'].values[0]-600
@@ -37,39 +37,39 @@ def yolo(image):
 # cv2.waitKey()
 # images = []
 file = open('test_corr.txt', 'w')
-for file_name in os.listdir('./new_data'):
-    split1 = file_name.split('(')
-    name = split1[0]
-    split2 = split1[1].split(')')
-    split3 = split2[0].split(',')
-    print(split3)
-    x = split3[0]
-    y = split3[1]
+for file_name in os.listdir('./new-new-data'):
+#     split1 = file_name.split('(')
+#     name = split1[0]
+#     split2 = split1[1].split(')')
+#     split3 = split2[0].split(',')
+#     print(split3)
+#     x = split3[0]
+#     y = split3[1]
     
-    img = cv2.imread(os.path.join("./new_data", file_name))
+    img = cv2.imread(os.path.join("./new-new-data", file_name))
     corr_data = yolo(img)
-    red_corr = corr_data[corr_data['name'] == 'red']
-    xcenter_pixel = red_corr['xcenter'].values[0]-607
-    ycenter_pixel = red_corr['ycenter'].values[0]-25
-    #67pixel = 3cm 
-    xcenter_mm = xcenter_pixel/62*30
-    xcenter_mm = round(xcenter_mm,2)
-    ycenter_mm = ycenter_pixel/62*30
-    ycenter_mm = round(ycenter_mm, 2)
+#     red_corr = corr_data[corr_data['name'] == 'red']
+#     xcenter_pixel = red_corr['xcenter'].values[0]-631
+#     ycenter_pixel = red_corr['ycenter'].values[0]-27
+#     #67pixel = 3cm 
+#     xcenter_mm = xcenter_pixel/62*30
+#     xcenter_mm = round(xcenter_mm,2)
+#     ycenter_mm = ycenter_pixel/62*30
+#     ycenter_mm = round(ycenter_mm, 2)
 
-    err_x = abs(int(x)-xcenter_mm)
-    err_x = round(err_x,2)
-    err_y = abs(int(y)-ycenter_mm) 
-    err_y = round(err_y,2)
-    import math
-    err = math.sqrt(err_x*err_x+err_y*err_y)
-    err = round(err, 2)
-    w = red_corr['width'].values[0]/62*30
-    w = round(err,2)
-    h = red_corr['height'].values[0]/62*30
-    h = round(err, 2)
+#     err_x = abs(int(x)-xcenter_mm)
+#     err_x = round(err_x,2)
+#     err_y = abs(int(y)-ycenter_mm) 
+#     err_y = round(err_y,2)
+#     import math
+#     err = math.sqrt(err_x*err_x+err_y*err_y)
+#     err = round(err, 2)
+#     w = red_corr['width'].values[0]/62*30
+#     w = round(err,2)
+#     h = red_corr['height'].values[0]/62*30
+#     h = round(err, 2)
    
-    file.writelines([name," ", str(x), " ", str(y), " ", str(xcenter_mm)," ", str(ycenter_mm),"\n"])
-    # print(file_name.split('(')[0])
-    # file.writelines([file_name, "       ",str(xcenter_mm), "        ", str(ycenter_mm),"\n"])
+#     # file.writelines([name," ", str(x), " ", str(y), " ", str(xcenter_mm)," ", str(ycenter_mm),"\n"])
+#     # print(file_name.split('(')[0])
+#     file.writelines([file_name, "       ",str(xcenter_mm), "        ", str(ycenter_mm),"\n"])
     
