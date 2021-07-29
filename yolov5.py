f9372b5bdd1c2265a82f121f95becbcf71251c6c
itachi176 <lhoang17062000@gmail.com>
2021-07-29 19:42:28 +0700

    update

--- yolov5.py
+++ yolov5.py
@@ -3,6 +3,8 @@ import torch
 import  cv2
 import torchvision
 import os 
+import ChessboardCalibration.tools.predict as predict #calibration 
+import time
 def yolo(image):
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='super-best.pt')
     model.conf = 0.6
@@ -10,7 +12,7 @@ def yolo(image):
     img1 = image[..., ::-1]
     results = model(img1)
     results.print()  
-    results.show()
+    # results.show()
     # results.save()
     results.xywh[0]
     a = results.pandas().xywh[0]
@@ -22,8 +24,9 @@ def yolo(image):
 # x = yolo()
 # y = x[x['name']=='red']
 # print(y['xcenter'].values[0])
-img = cv2.imread('./anhnam/a.jpg')
-corr_data = yolo(img)
+# img = cv2.imread('./anhnam/a.jpg')
+# predict.pred(754.3876342773438, 546.3013916015625, "./a.jpg")
+# corr_data = yolo(img)
 # red_corr = corr_data[corr_data['name'] == 'red']
 # xcenter_pixel = red_corr['xcenter'].values[0]-609
 # ycenter_pixel = red_corr['ycenter'].values[0]-28
@@ -36,43 +39,44 @@ corr_data = yolo(img)
 # cv2.imshow('aaa', img)
 # cv2.waitKey()
 # images = []
-# file = open('test_corr.txt', 'w')
-# for file_name in os.listdir('./anhnam'):
-#     split1 = file_name.split('(')
-#     name = split1[0]
-#     split2 = split1[1].split(')')
-#     split3 = split2[0].split(',')
-#     print(split3)
-#     x = split3[0]
-#     y = split3[1]
+file = open('test_corr.txt', 'w')
+for file_name in os.listdir('./anhdung'):
+    split1 = file_name.split('(')
+    name = split1[0]
+    split2 = split1[1].split(')')
+    split3 = split2[0].split(',')
+    print(split3)
+    x = split3[0]
+    y = split3[1]
     
-#     img = cv2.imread(os.path.join("./anhnam", file_name))
-#     corr_data = yolo(img)
-#     try:
-#         red_corr = corr_data[corr_data['name'] == 'red']
-#         xcenter_pixel = red_corr['xcenter'].values[0]-625
-#         ycenter_pixel = red_corr['ycenter'].values[0]-34
-#         #67pixel = 3cm 
-#         xcenter_mm = xcenter_pixel/62*30
-#         xcenter_mm = round(xcenter_mm,2)
-#         ycenter_mm = ycenter_pixel/62*30
-#         ycenter_mm = round(ycenter_mm, 2)
+    img = cv2.imread(os.path.join("./anhdung", file_name))
+    corr_data = yolo(img)
+   
+    red_corr = corr_data[corr_data['name'] == 'red']
+    xcenter_pixel = red_corr['xcenter'].values[0]
+    ycenter_pixel = red_corr['ycenter'].values[0]
+    #67pixel = 3cm 
+    # time.sleep(2)
+    xcenter_mm, ycenter_mm = predict.pred(xcenter_pixel, ycenter_pixel, os.path.join("./anhdung", file_name))
+    # xcenter_mm = xcenter_pixel/62*30
+    xcenter_mm = round(xcenter_mm,2)*10
+    # ycenter_mm = ycenter_pixel/62*30
+    ycenter_mm = round(ycenter_mm, 2)*10
 
-#         err_x = abs(int(x)-xcenter_mm)
-#         err_x = round(err_x,2)
-#         err_y = abs(int(y)-ycenter_mm) 
-#         err_y = round(err_y,2)
-#         import math
-#         err = math.sqrt(err_x*err_x+err_y*err_y)
-#         err = round(err, 2)
-#         w = red_corr['width'].values[0]/62*30
-#         w = round(err,2)
-#         h = red_corr['height'].values[0]/62*30
-#         h = round(err, 2)
+    err_x = abs(int(x)-xcenter_mm)
+    err_x = round(err_x,2)
+    err_y = abs(int(y)-ycenter_mm) 
+    err_y = round(err_y,2)
+    import math
+    err = math.sqrt(err_x*err_x+err_y*err_y)
+    err = round(err, 2)
+    w = red_corr['width'].values[0]/62*30
+    w = round(err,2)
+    h = red_corr['height'].values[0]/62*30
+    h = round(err, 2)
+
+    file.writelines([name," ", str(x), " ", str(y), " ", str(err_x), " ", str(err_y), " ", str(err),"\n"])
     
-#         file.writelines([name," ", str(x), " ", str(y), " ", str(xcenter_mm)," ", str(ycenter_mm)," ", str(err_x), " ", str(err_y), " ", str(err),"\n"])
-#     except:
-#         pass
 # #     # print(file_name.split('(')[0])
 # #     # file.writelines([file_name, "       ",str(xcenter_mm), "        ", str(ycenter_mm),"\n"])
     
