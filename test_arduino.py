6bb4d6f95fda74677dac43570d8557d3a1865436
itachi176 <lhoang17062000@gmail.com>
2021-07-30 03:42:56 +0700

    fix bug capture image

--- test_arduino.py
+++ test_arduino.py
@@ -2,7 +2,7 @@ import torch
 import  cv2
 import os 
 import ChessboardCalibration_master.tools.predict as predict #calibration 
-
+import time
 
 def yolo(image):
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='super-best.pt')
@@ -22,35 +22,44 @@ def yolo(image):
     return a 
 
 
-def get_corr(img):
+def get_corr(img_path):
+    img = cv2.imread(img_path)
     corr_data = yolo(img)
     red_corr = corr_data[corr_data['name'] == 'red']
     xcenter_pixel = red_corr['xcenter'].values[0]
     ycenter_pixel = red_corr['ycenter'].values[0]
     #67pixel = 3cm 
     # time.sleep(2)
-    xcenter_mm, ycenter_mm = predict.pred(xcenter_pixel, ycenter_pixel, img)
+    xcenter_mm, ycenter_mm = predict.pred(xcenter_pixel, ycenter_pixel, img_path)
     # xcenter_mm = xcenter_pixel/62*30
     xcenter_mm = round(xcenter_mm,2)*10
     # ycenter_mm = ycenter_pixel/62*30
     ycenter_mm = round(ycenter_mm, 2)*10
-    err_x = abs(int(x)-xcenter_mm)
-    err_x = round(err_x,2)
-    err_y = abs(int(y)-ycenter_mm) 
-    err_y = round(err_y,2)
+    # err_x = abs(int(x)-xcenter_mm)
+    # err_x = round(err_x,2)
+    # err_y = abs(int(y)-ycenter_mm) 
+    # err_y = round(err_y,2)
     import math
-    err = math.sqrt(err_x*err_x+err_y*err_y)
-    err = round(err, 2)
+    # err = math.sqrt(err_x*err_x+err_y*err_y)
+    # err = round(err, 2)
     return "{} {}".format(xcenter_mm, ycenter_mm)
     
 cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
-
+    if ret == False:
+        break
+    k = cv2.waitKey(1)
+    
     cv2.imshow("hi", frame)
-    if 0xFF == ord('c'):
-        cv2.imwrite('./robot_cam/', frame)
-        str = get_corr(frame)
+    # a = input()
+    # if 0xFF == ord('c'):
+    
+    if k%256 == 27:
+        # ESC pressed
+        str = get_corr('./a.jpg')
+        print(str)
+        print("hello")
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
