cf5d056c8534c6b832e98b2f38ac70d062dedc07
itachi176 <lhoang17062000@gmail.com>
2021-09-21 02:17:56 +0700

    update

--- gesture/hand_tracking.py
+++ gesture/hand_tracking.py
@@ -81,7 +81,7 @@ class handDetector():
         
     def finger(self):
         fingers = []
-        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0]-1][1]:
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0]-1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
@@ -94,6 +94,8 @@ class handDetector():
             # print(self.lmList[self.tipIds[id]][2], self.lmList[self.tipIds[id]-2][2])
             
         return fingers
+
+    
 # mpHands = mp.solutions.hands
 # hands = mpHands.Hands()
 # mpDraw = mp.solutions.drawing_utils
@@ -104,8 +106,8 @@ def main():
     detector = handDetector()
     ptime = 0
     xp, yp = 0, 0
-    imgCanvas = np.ones((480, 640, 3), np.uint8)
-    model = tf.keras.models.load_model('./gesture/ml.h5')
+    # imgCanvas = np.ones((480, 640, 3), np.uint8)
+    # model = tf.keras.models.load_model('./gesture/ml.h5')
     while(True):
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
@@ -115,27 +117,32 @@ def main():
         if len(lmList) != 0:
             # print(lmList[8])
             finger = detector.finger()
-            # print(finger)
-            if finger[1] and finger[2] == False:
-                # print("select mode")
-                imgCanvas = np.zeros((480, 640, 3), np.uint8)
-                
-            if finger[1] and finger[2] and finger[3] and finger[4]:
-                frame = cv2.circle(frame, (lmList[8][1], lmList[8][2]), 20, (255,0,0), cv2.FILLED)
-                if xp ==0 and yp ==0:
-                    xp, yp = lmList[8][1], lmList[8][2]
+            
+            
 
-                cv2.line(frame, (xp, yp), (lmList[8][1], lmList[8][2]), (0, 0, 255), 20 )
-                cv2.line(imgCanvas, (xp, yp), (lmList[8][1], lmList[8][2]), (0, 0, 255), 20)
-                xp, yp = lmList[8][1], lmList[8][2]
+            # print(finger[2])
+        
+            # print(finger)
+            # if finger[1] and finger[2] == False:
+                # print("select mode")
+                # imgCanvas = np.zeros((480, 640, 3), np.uint8)
+                
+            # if finger[1] and finger[2] and finger[3] and finger[4]:
+                # frame = cv2.circle(frame, (lmList[8][1], lmList[8][2]), 20, (255,0,0), cv2.FILLED)
+                # if xp ==0 and yp ==0:
+                #     xp, yp = lmList[8][1], lmList[8][2]
+
+                # cv2.line(frame, (xp, yp), (lmList[8][1], lmList[8][2]), (0, 0, 255), 20 )
+                # cv2.line(imgCanvas, (xp, yp), (lmList[8][1], lmList[8][2]), (0, 0, 255), 20)
+                # xp, yp = lmList[8][1], lmList[8][2]
 
                 # img = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
                 
         
-        imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
-        _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
-        imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
-        frame = cv2.bitwise_and(frame, imgInv)
+        # imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
+        # _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
+        # imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+        # frame = cv2.bitwise_and(frame, imgInv)
         # frame = cv2.bitwise_or(frame, imgCanvas)
         #display fps
         ctime = time.time()
@@ -150,20 +157,32 @@ def main():
 
             # print("drawing")
             # print(img.shape)
-            cv2.imwrite("./test4.png", imgCanvas)
+            # cv2.imwrite("./test4.png", imgCanvas)
+            break
 
         #predict
-        try:
-            final = cv2.imread("test4.png")
-            result = pred(final)
-            # print("du doan:", result)
-            cv2.putText(frame, f'Predict: {result}',(0, 70), cv2.FONT_HERSHEY_PLAIN,
-                    3, (255, 255, 0), 3)
-                # break
-        except:
-            pass
-        cv2.imshow("draw", imgInv)
+        # try:
+        #     final = cv2.imread("test4.png")
+        #     result = pred(final)
+        #     # print("du doan:", result)
+        #     cv2.putText(frame, f'Predict: {result}',(0, 70), cv2.FONT_HERSHEY_PLAIN,
+        #             3, (255, 255, 0), 3)
+        #         # break
+        # except:
+        #     pass
+        
+        
         cv2.imshow("image", frame)
+        if cv2.waitKey(33) == ord("a"):
+            if not finger[1] and not finger[2] and not finger[3] and not finger[4] and finger[0]:
+                print("006")
+            if finger[1] and finger[2] and finger[3] and finger[4] and finger[0]:
+                print("003")
+            # print(finger)
+            if finger[1] and finger[2] and not finger[3] and not finger[4] and not finger[0]:
+                print("005")
+            elif finger[1] and not finger[2] and not finger[3] and not finger[4] and not finger[0]:
+                print("004")
     cap.release()
     cv2.destroyAllWindows()
 
--- gesture/test.py
+++ gesture/test.py
@@ -1,47 +1,77 @@
-import tensorflow as tf
+# import necessary packages
+
+import cv2
 import numpy as np
-model = tf.keras.models.load_model('./gesture/ml.h5')
-import cv2 
-def pred(img):
-# img = cv2.imread('./test4.png')
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, imgInv = cv2.threshold(gray_img, 50, 255, cv2.THRESH_BINARY_INV)
-    gray_img = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
-    gray_img = cv2.cvtColor(gray_img, cv2.COLOR_BGR2GRAY)
-    # img = cv2.resize(img, (28, 28))
-    # img = np.pad(img, (10,10), 'constant', constant_values=0 )
-    # img = cv2.resize(img, (28,28))/255
-    # a = tf.expand_dims(img, axis=0)
-    # x = model.predict(a)
-    # y = np.argmax(x)
-    # print(y)
-    contours, hierarchy = cv2.findContours(gray_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Sắp xếp các contour theo diện tích giảm dần:
-    area_cnt = [cv2.contourArea(cnt) for cnt in contours]
-    area_sort = np.argsort(area_cnt)[::-1]
-    # Top 20 contour có diện tích lớn nhất
-    print("area:", area_sort)
-    def _drawBoundingBox(img, cnt):
-        x,y,w,h = cv2.boundingRect(cnt)
-        img = cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
-        new_img = img[y:y+h, x:x+w]
-        return new_img
-    new_img = _drawBoundingBox(img, contours[1])
-    new_img= cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-    new_img = cv2.resize(new_img, (28, 28))
-    new_img = np.pad(new_img, (10,10), 'constant', constant_values=0 )
-    new_img = cv2.resize(new_img, (28,28))/255
-    print(new_img.shape)
-    a = tf.expand_dims(new_img, axis=0)
-    x = model.predict(a)
-    y = np.argmax(x)
-    print(y)
-    return y
-# img = cv2.imread('./test4.png')
-# print(pred(img))
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+# initialize mediapipe
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
+
+# Load the gesture recognizer model
+model = load_model('./gesture/mp_hand_gesture')
+
+# Load class names
+f = open('./gesture/gesture.names', 'r')
+classNames = f.read().split('\n')
+f.close()
+print(classNames)
 
 
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
 
-# print(img.shape)
-# cv2.imshow('aaa', new_img)
-# cv2.waitKey()
+while True:
+    # Read each frame from the webcam
+    _, frame = cap.read()
+
+    x, y, c = frame.shape
+
+    # Flip the frame vertically
+    frame = cv2.flip(frame, 1)
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Get hand landmark prediction
+    result = hands.process(framergb)
+
+    # print(result)
+    
+    className = ''
+
+    # post process the result
+    if result.multi_hand_landmarks:
+        landmarks = []
+        for handslms in result.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                # print(id, lm)
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
+
+                landmarks.append([lmx, lmy])
+
+            # Drawing landmarks on frames
+            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+
+            # Predict gesture
+            prediction = model.predict([landmarks])
+            # print(prediction)
+            classID = np.argmax(prediction)
+            className = classNames[classID]
+
+    # show the prediction on the frame
+    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (0,0,255), 2, cv2.LINE_AA)
+
+    # Show the final output
+    cv2.imshow("Output", frame) 
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# release the webcam and destroy all active windows
+cap.release()
+
+cv2.destroyAllWindows()
